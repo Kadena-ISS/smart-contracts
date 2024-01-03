7769d32ab785d92b6a02fe76597b34ed5ffcf455
Dmytro Sotnyk <dmytro.sotnyk@ideasoft.io>
2024-01-03 18:55:32 +0200

    updated pact deployment

--- typescript/deploy-bridge.ts
+++ typescript/deploy-bridge.ts
@@ -1,30 +1,76 @@
-import { deployGasOracle } from "./deploy-modules";
-import { defineKeyset, deployInterfaces, deployStructs } from "./deploy-utils";
+import {
+  deployGasOracle,
+  deployIGP,
+  deployISM,
+  deployMailbox,
+  deployValidatorAnnounce,
+} from "./deploy-modules";
+import { deployInterfaces, deployStructs } from "./deploy-utils";
 import { IKeypair, createClient } from "@kadena/client";
+import {
+  IAccountWithPublicKey,
+  defineKeyset,
+  fundAccount,
+} from "./kadena-utils";
 
 async function main() {
   const devnet_url = `http://localhost:8080/chainweb/0.0/fast-development/chain/0/pact`;
 
-  const sender00Pk =
-    "368820f80c324bbc7c2b0610688a7da43e39f91d118732671cd9c7500ff43cca";
-  const sender00Sk =
-    "251a920c403ae8c8f65f59142316af3c82b631fba46ddea92ee8c95035bd2898";
-
-  const keys: IKeypair = {
-    publicKey: sender00Pk,
-    secretKey: sender00Sk,
+  const s_keys: IKeypair = {
+    publicKey:
+      "368820f80c324bbc7c2b0610688a7da43e39f91d118732671cd9c7500ff43cca",
+    secretKey:
+      "251a920c403ae8c8f65f59142316af3c82b631fba46ddea92ee8c95035bd2898",
   };
-  const keysetName = "bridge-admin";
+
+  const b_keys: IKeypair = {
+    publicKey:
+      "f833278f8825f553c5dcde489af6b982d873947375130ab452a6b6bee071a059",
+    secretKey:
+      "9396ad2d586b90a7d4a1ab2a0cc55e9d04fc6df7ff7c9c9f41cc8c2cbe3a4d0c",
+  };
+
+  const b_account: IAccountWithPublicKey = {
+    name: "bridge-admin",
+    publicKey: b_keys.publicKey,
+  };
+
+  const sender00Keyset = "ks";
+  const bridgeAdminKeyset = "bridge-admin";
 
   const client = createClient(devnet_url);
 
-  await defineKeyset(client, keys, keysetName);
+  console.log('Defining keyset "sender00"');
+  // await defineKeyset(client, s_keys, sender00Keyset, "sender00");
 
-  await deployStructs(client, keys, keysetName);
+  // await fundAccount(client, s_keys, sender00Keyset, b_account, 100);
 
-  await deployInterfaces(client, keys, keysetName);
+  console.log('Defining keyset "bridge-admin"');
+  await defineKeyset(client, b_keys, bridgeAdminKeyset, "bridge-admin");
 
-  await deployGasOracle(client, keys, keysetName);
+  // console.log("\nDeploying structs");
+  // await deployStructs(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying coin");
+  // await deployCoin(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying interfaces");
+  // await deployInterfaces(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying GasOracle");
+  // await deployGasOracle(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying ValidatorAnnounce");
+  // await deployValidatorAnnounce(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying ISM");
+  // await deployISM(client, s_keys, sender00Keyset, 1);
+
+  // console.log("\nDeploying IGP");
+  // await deployIGP(client, s_keys, sender00Keyset);
+
+  // console.log("\nDeploying Mailbox");
+  // await deployMailbox(client, s_keys, sender00Keyset);
 }
 
 main();
--- typescript/deploy-utils.ts
+++ typescript/deploy-utils.ts
@@ -1,19 +1,7 @@
 import { IClient, IKeypair } from "@kadena/client";
 import * as fs from "fs";
-import { submitSignedTx } from "./execute";
+import { submitSignedTx } from "./kadena-utils";
 import path from "path";
-
-export const defineKeyset = async (
-  client: IClient,
-  keys: IKeypair,
-  keysetName: string
-) => {
-  const command = `(namespace "free")
-  (define-keyset "free.bridge-admin" (read-keyset '${keysetName}))`;
-
-  const result = await submitSignedTx(client, keys, keysetName, command);
-  console.log(result);
-};
 
 export const deployStructs = async (
   client: IClient,
@@ -24,32 +12,46 @@ export const deployStructs = async (
   const folder = await fs.promises.opendir(folderName);
   for await (const dirent of folder) {
     const fileName = path.join(folderName, dirent.name);
-    console.log(fileName)
+    console.log(fileName);
     const file = (await fs.promises.readFile(fileName)).toString();
     const result = await submitSignedTx(client, keys, keysetName, file);
     console.log(result);
   }
 };
 
-export const deployCoin = async (
-  client: IClient,
-  keys: IKeypair,
-  keysetName: string
-) => {
+// export const deployCoin = async (
+//   client: IClient,
+//   keys: IKeypair,
+//   keysetName: string
+// ) => {
+//   const fungibleV2Iface = (
+//     await fs.promises.readFile("../pact/interfaces/fungible-v2.pact")
+//   ).toString();
+//   const fungibleV2Result = await submitSignedTx(
+//     client,
+//     keys,
+//     keysetName,
+//     fungibleV2Iface
+//   );
+//   console.log(fungibleV2Result);
 
-  const fungibleV2Iface = (await fs.promises.readFile("../pact/interfaces/fungible-v2.pact")).toString();
-  const fungibleV2Result = await submitSignedTx(client, keys, keysetName, fungibleV2Iface);
-  console.log(fungibleV2Result);
+//   const fungibleXchainIface = (
+//     await fs.promises.readFile("../pact/coin/fungible-xchain-v1.pact")
+//   ).toString();
+//   const fungibleXchainResult = await submitSignedTx(
+//     client,
+//     keys,
+//     keysetName,
+//     fungibleXchainIface
+//   );
+//   console.log(fungibleXchainResult);
 
-  const fungibleXchainIface = (await fs.promises.readFile("../pact/coin/fungible-xchain-v1.pact")).toString();
-  const fungibleXchainResult = await submitSignedTx(client, keys, keysetName, fungibleXchainIface);
-  console.log(fungibleXchainResult);
-
-  const coin = (await fs.promises.readFile("../pact/coin/coin-v5.pact")).toString();
-  const coinResult = await submitSignedTx(client, keys, keysetName, coin);
-  console.log(coinResult);
-};
-
+//   const coin = (
+//     await fs.promises.readFile("../pact/coin/coin-v5.pact")
+//   ).toString();
+//   const coinResult = await submitSignedTx(client, keys, keysetName, coin);
+//   console.log(coinResult);
+// };
 
 export const deployInterfaces = async (
   client: IClient,
