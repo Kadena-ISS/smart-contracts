4764333f351918e5474af8b3ebb5ad7e87f33aca
Dmytro Sotnyk <dmytro.sotnyk@ideasoft.io>
2024-01-29 17:12:28 +0200

    promises speed up

--- typescript/scripts/deploy-core-bridge.ts
+++ typescript/scripts/deploy-core-bridge.ts
@@ -16,23 +16,24 @@ import {
 import { deployAccounts } from "./deploy-accounts";
 
 async function main() {
-
   await defineKeyset(clientWData, s_account);
 
-  await deployAccounts(clientWData);
-  await deployAccounts(clientWData1);
+  Promise.all([deployAccounts(clientWData), deployAccounts(clientWData1)]);
 
   await deployStructs(clientWData, s_account);
   await deployInterfaces(clientWData, s_account);
 
-  await deployGasOracle(clientWData, b_account);
-  await deployValidatorAnnounce(clientWData, b_account);
+  Promise.all([
+    deployGasOracle(clientWData, b_account),
+    deployValidatorAnnounce(clientWData, b_account),
+  ]);
 
   const validators = ["0x71239e00AE942B394B3a91ab229E5264aD836f6f"];
   const threshold = 1;
-  await deployISM(clientWData, b_account, validators, threshold);
-
-  await deployIGP(clientWData, b_account);
+  Promise.all([
+    deployISM(clientWData, b_account, validators, threshold),
+    deployIGP(clientWData, b_account),
+  ]);
   await deployMailbox(clientWData, b_account);
 }
 
